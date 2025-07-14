46908ef909781ac416d719835db51a7c8a895756
Oybek <aquarius280186@gmail.com>
2025-07-14 09:49:44 +0500

    meta tag seo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="googledb351a4060d01e97"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
